aa052aa183b6048396768a6c61cc9510c3aa91dd
matheuslssouza <matheuslssouza@usp.br>
2025-01-30 15:36:22 -0300

    Integrate the generated secret key with the algorithm

---
+++ src/main/java/com/FileEncrypter.java
@@ -0,0 +1,63 @@
+package com;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.io.*;
+
+
+public class FileEncrypter {
+
+    private static final int BUFFER_SIZE = 1024;
+    private static final int END_OF_FILE = -1;
+    private static final String ALGORITHM = "AES";
+    private static final String TRANSFORMATION = "AES/ECB/PKCS5Padding";
+    private static final int CIPHER_MODE = Cipher.ENCRYPT_MODE;
+    private final String inputFilePath;
+    private final String outputFilePath;
+
+    // Constructor
+    public FileEncrypter(String inputFilePath) {
+        this.inputFilePath = inputFilePath.replace('\\', '/');
+        this.outputFilePath = this.inputFilePath + ".enc";
+        processFile();
+    }
+
+    // Process File to Encrypt
+    public void processFile() {
+        try {
+            FileInputStream fis = new FileInputStream(inputFilePath);
+            FileOutputStream fos = new FileOutputStream(outputFilePath);
+
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int bytesRead;
+
+            while ((bytesRead = fis.read(buffer)) != END_OF_FILE) {
+                fos.write(buffer, 0, bytesRead);
+            }
+
+            System.out.println("File successfully processed: " + outputFilePath);
+
+            SecretKey AESGeneratedKey = generateAESKey();
+            Cipher cipher = createCipher(CIPHER_MODE, AESGeneratedKey);
+
+        } catch (Exception e) {
+            System.err.println("Error processing file: " + e.getMessage());
+        }
+    }
+
+    // Generate secret key to use in algorithm
+    private SecretKey generateAESKey() throws Exception {
+        KeyGenerator secretKey = KeyGenerator.getInstance(ALGORITHM);
+        secretKey.init(128);
+        return secretKey.generateKey();
+    }
+
+    // Algorithm to encrypt (For more security, change for CBC mode in TRANSFORMATION)
+    private Cipher createCipher(int mode, SecretKey AESGeneratedKey) throws Exception {
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        cipher.init(mode, AESGeneratedKey);
+        return cipher;
+    }
+
+}
--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,7 +1,25 @@
 package com;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        Encrypter encrypter = new Encrypter();
+        System.out.println("Encrypt and Decrypt program, be careful! ");
+        System.out.println("Press 1 for encrypt, 2 for decrypt and 3 for exit: ");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        switch (choice){
+            case 1:
+                System.out.println("Insert the link of archive to encrypt");
+                System.out.println("Link example: C:/Users/Documentos/Ransomware/teste.txt");
+                Scanner scanner1 = new Scanner(System.in);
+                String directoryForEncrypt = scanner1.nextLine();
+                FileEncrypter encrypter = new FileEncrypter(directoryForEncrypt);
+            case 2:;
+            case 3: break;
+        }
+
     }
 }
